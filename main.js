fcaa2f79b937b148eb1e7e882a542b5724e3efcb
Goldenbrain1 <143943444+Goldenbrain1@users.noreply.github.com>
2023-10-21 13:23:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('Welcome to our site!');
+function Location() {
+   alert ('We are located at Ado, Nasarawa State.')
+  }
+  function quality() {
+   alert('YES! we offer the best quality service here in Nasarawa State, Nigeria.')
+  }
+  function park() {
+   alert('We have more than enough parking space.')
+  }
+  function Send() {
+   alert('Thanks for contacting us. Your message has been recieved and you will get feedback shortly!')
+  }
